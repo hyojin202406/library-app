f6f2d797b609daef170ab894e71f6c16f05ab60d
hyojin202406 <hyojin202406@gmail.com>
2025-01-29 16:34:09 +0900

    feat/refactor: 서비스 계층을 코틀린으로 변경하기

--- src/main/kotlin/com/group/libraryapp/utill/ExceptionUtills.kt
+++ src/main/kotlin/com/group/libraryapp/utill/ExceptionUtills.kt
@@ -1,5 +1,13 @@
 package com.group.libraryapp.utill
 
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
+
 fun fail(): Nothing {
     throw IllegalArgumentException()
+}
+
+// 확장함수
+fun <T, ID> CrudRepository<T, ID>.findByIdOrThrow(id: ID): T {
+    return this.findByIdOrNull(id) ?: fail()
 }
