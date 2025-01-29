1c280856fb537e8e919264678457cd9ff467cc2d
hyojin202406 <hyojin202406@gmail.com>
2025-01-30 05:00:02 +0900

    feat/refactor: 코틀린 리팩토링

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -7,7 +7,6 @@ import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
 import com.group.libraryapp.utill.fail
 import com.group.libraryapp.utill.findByIdOrThrow
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -25,7 +24,7 @@ class UserService(
     @Transactional(readOnly = true)
     fun getUsers(): List<UserResponse> {
         return userRepository.findAll()
-            .map(::UserResponse)
+            .map{ user -> UserResponse.of(user) }
     }
 
     @Transactional
